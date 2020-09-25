a56b23224f74b1a7d083567b3d25dd9029c3be64
Kate Skorija <kateskorija@gmail.com>
2020-09-25 10:04:53 -0700

    create initial component files and add routes to app.js

---
+++ src/components/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Explore from './Explore';
+import Login from './Login';
+import Register from './Register';
+import Logout from './Logout';
+import Splash from './Splash';
+import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Switch>
+      <Route path="/register">
+        <Register />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/logout">
+        <Logout />
+      </Route>
+      <Route path="/explore">
+        <Explore />
+      </Route>
+      <Route path="/myplaces">
+        <MyPlaces />
+      </Route>
+      <Route path="/">
+        <Splash />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+  );
+}
+
+export default App;

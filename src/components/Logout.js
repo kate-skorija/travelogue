16cc1eb0bb166a1ad607a9c43ca7041fbf4278a6
Kate Skorija <kateskorija@gmail.com>
2020-09-25 10:49:37 -0700

    build logout component

---
+++ src/components/Logout.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import firebase from "firebase/app";
+
+function Logout() {
+
+  const [loggedIn, setLoggedIn] = useState(true)
+
+  function doLogout() {
+    firebase.auth().signOut().then(function() {
+      console.log("Signed out!");
+      setLoggedIn(false);
+    }).catch(function(error) {
+      alert(error.message) ;
+    });
+  }
+
+  return (
+      <React.Fragment>
+        <Nav />
+        <h3>Thanks for using Travelogue. See you again soon.</h3>
+        {loggedIn ? <button onClick={doLogout}>Log Out</button> : <h4>You have successfully logged out!</h4>}
+      </React.Fragment>
+  )
+}
+
+export default Logout;

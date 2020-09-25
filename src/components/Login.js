3b3ab62f999e2ba8c1892a15a409974e913c38ff
Kate Skorija <kateskorija@gmail.com>
2020-09-25 10:24:22 -0700

    build login component

---
+++ src/components/Login.js
@@ -0,0 +1,38 @@
+import React from "react";
+import firebase from "firebase/app";
+import { withRouter } from 'react-router-dom';
+
+const Login = ({history}) => {  
+
+  function doLogin(event) {
+    event.preventDefault();
+    const email = event.target.signinEmail.value;
+    const password = event.target.signinPassword.value;
+    firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+      history.push('/')
+    }).catch(function(error) {
+      alert(error.message) 
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <h1>Log In</h1>
+      <form onSubmit={doLogin}>
+        <input
+          type='text'
+          name='signinEmail'
+          placeholder='email' />
+        <br/>
+        <input
+          type='password'
+          name='signinPassword'
+          placeholder='Password' />
+        <br/>
+        <button type='submit'>Log In</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+export default withRouter(Login);
